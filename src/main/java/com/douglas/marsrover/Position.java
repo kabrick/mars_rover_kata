31f94b9f0aedcdca63a867979b869f767f4a83e3
kabrick <kabuyedouglas53@gmail.com>
2019-08-21 09:32:04 +0300

    move the rover forward and backwards

--- src/main/java/com/douglas/marsrover/Position.java
+++ src/main/java/com/douglas/marsrover/Position.java
@@ -24,4 +24,16 @@ public class Position {
     public void setMaximumPosition(int maximum_position) {
         this.maximum_position = maximum_position;
     }
+
+    public int getForwardPosition() {
+        return (getPosition() + 1) % (getMaximumPosition() + 1);
+    }
+
+    public int getBackwardPosition() {
+        if (getPosition() > 0){
+            return getPosition() - 1;
+        } else {
+            return getMaximumPosition();
+        }
+    }
 }
